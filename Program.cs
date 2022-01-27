5e1564c2b749f56e249fd3af2ea03477c02a635e
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 17:57:06 +0530

    Adding first program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsForNUnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to the programs for N unit testing");
+            Console.ReadLine();
+
+        }
+    }
+}
