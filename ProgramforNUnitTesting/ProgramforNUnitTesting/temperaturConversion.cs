75c4b82600da9447c141072e6ca901d2a0233415
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-01 00:24:55 +0530

    Adding Program For NUnit Testing

---
+++ ProgramforNUnitTesting/ProgramforNUnitTesting/temperaturConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramforNUnitTesting
+{
+    internal class temperaturConversion
+    {
+        public void tempConversion(double celsius, double fahrenheit)
+
+        {
+           
+            double celToFah = (celsius * 9 / 5) + 32;
+            double fahToCel = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("After celsius to fahrenheit conversion: {0}", celToFah);
+            Console.WriteLine("After fahrenheit to celsius conversion: {0}", fahToCel);
+        }
+    }
+
+}
