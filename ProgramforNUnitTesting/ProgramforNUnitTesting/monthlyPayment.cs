bf5c8f7153e3b70f3f7ad15e83c071e264588ba7
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-01 00:21:13 +0530

    Adding Program for NUnit Testing

---
+++ ProgramforNUnitTesting/ProgramforNUnitTesting/monthlyPayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramforNUnitTesting
+{
+    internal class monthlyPayment
+    {
+
+        public void MonPayment(double p, double y, double R)
+        {
+            double n = 12 * y;
+            double r = R / (12 * 100);
+            double payment = p * r / (1 - Math.Pow((1 + r), -n));
+            Console.WriteLine(payment + "is monthly payment");
+        }
+    }
+}
